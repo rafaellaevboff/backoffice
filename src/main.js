0fb58599cba3483b1357d9570bed6ec9bc8b47bd
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2024-04-12 21:04:24 -0300

    Corrige conexão vuetify 3 e detalhes da tela de login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  ssr: true
+})
+
 createApp(App)
 .use(router)
+.use(vuetify)
 .mount('#app')
--- src/views/LoginUsuario.vue
+++ src/views/LoginUsuario.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
       <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
-        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+        <div class="text-subtitle-1 text-medium-emphasis">Login</div>
   
-        <v-text-field density="compact" placeholder="Email address" prepend-inner-icon="mdi-email-outline"
+        <v-text-field density="compact" placeholder="Email" prepend-inner-icon="mdi-email-outline"
           variant="outlined"></v-text-field>
   
         <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
           Password
   
           <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer" target="_blank">
-            Forgot login password?</a>
+            Esqueci a senha</a>
         </div>
   
         <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
@@ -18,14 +18,8 @@
           @click:append-inner="visible = !visible"></v-text-field>
   
         <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
-          Log In
+          Entrar
         </v-btn>
-  
-        <v-card-text class="text-center">
-          <a class="text-blue text-decoration-none" href="#" rel="noopener noreferrer" target="_blank">
-            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-          </a>
-        </v-card-text>
       </v-card>
     </div>
   </template>
